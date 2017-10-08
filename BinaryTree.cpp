e34585f6d3bb89f15c0d3a8cae8e693c5bcdbce8
chaihahaha <836275709@qq.com>
2017-10-08 15:26:11 +0800

    2017.10.8

---
+++ BinaryTree.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+typedef int ElemType;
+class BTNode
+{
+public:
+        BTNode(ElemType data) {//创建有给定值的树根
+                this->data = data;
+                this->left = NULL;
+                this->right = NULL;
+        }
+        template<class T>
+        BTNode(T &A,T &B) {//用先序序列A和中序序列B创建一整个树
+                BTNode * root = PreInCreateOverLoad(A,B,1,sizeof(A)/sizeof(A[0]) - 1,1,sizeof(B)/sizeof(B[0]) - 1);
+                this->data = root->data;
+                this->left = root->left;
+                this->right = root->right;
+        }
+        void InPrint() {//打印中序序列
+                this->InPrintOverLoad();
+                cout<<endl;
+        }
+        
+        void PrePrint() {//打印先序序列
+                this->PrePrintOverLoad();
+                cout<<endl;
+        }
+        ElemType data;
+        BTNode * left;
+        BTNode * right;
+private:
+        void PrePrintOverLoad() {
+                BTNode * p = this;
+                if(p) {
+                        cout<<p->data<<" ";
+                        p->left->PrePrintOverLoad();
+                        p->right->PrePrintOverLoad();
+                }
+        }
+        void InPrintOverLoad() {
+                BTNode * p = this;
+                if(p) {
+                        p->left->InPrintOverLoad();
+                        cout<<p->data<<" ";
+                        p->right->InPrintOverLoad();
+                }
+        }
+        BTNode * PreInCreateOverLoad(ElemType A[], ElemType B[], int l1,int r1,int l2,int r2) {
+                BTNode *root = new BTNode(A[l1]);
+                int i = l2;
+                while(B[i] != root->data)
+                        i++;
+                int llen = i-l2;
+                int rlen = r2 - i;
+                if(llen)
+                        root->left = PreInCreateOverLoad(A,B,l1+1,l1+llen,l2,l2+llen - 1);
+                else
+                        root->left = NULL;
+                if(rlen)
+                        root->right = PreInCreateOverLoad(A,B,r1-rlen+1,r1,r2-rlen+1,r2);
+                else
+                        root->right = NULL;
+                return root;
+        }
+};
+typedef BTNode *BinaryTree;
---
+++ main_tree.cpp
@@ -0,0 +1,8 @@
+#include "BinaryTree.cpp"
+int main() {
+        int a[] = {0,1,2,3,4,5,6,7,8,9};
+        int b[] = {0,2,3,1,5,4,7,8,6,9};
+        BinaryTree t = new BTNode(a,b);
+        t->PrePrint();
+        t->InPrint();
+}
