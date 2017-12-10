58c8913da1bb12de52f22c2ba6a1a4d028f86098
chaihahaha <836275709@qq.com>
2017-12-10 10:33:33 -0500

    12.10

--- EvenOddSort.cpp
+++ EvenOddSort.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 using namespace std;
 void EvenOddSort(int A[]){
-    int i=1,j=A[0];
+    //不使用额外空间，时间复杂度O(n)，完成对数组内奇数和偶数的排序，将所有偶数排在奇数前面
+    int n=A[0];    //n用于在排序后恢复A[0]保存的元素个数
+    int i=1,j=n;
     while(i<j) {
         A[0]=A[i];
         while(A[j]%2 == 1 && i<j) j--;
@@ -10,11 +12,12 @@ void EvenOddSort(int A[]){
         if(i<j) { A[j] = A[i]; j--; }
         A[i]=A[0];
     }
+    A[0]=n;
 }
 int main() {
-    int A[11]={10,6,3,9,2,7,5,8,4,1,10};
+    int A[11]={10,6,3,9,2,7,5,8,4,1,10};//A[0]保存元素个数
     EvenOddSort(A);
-    for(int i=0; i < 10; i++){
+    for(int i=1; i <= A[0]; i++){
         cout<<"index:"<<i+1<<"  "<<"value:"<<A[i]<<endl;
     }
 }
