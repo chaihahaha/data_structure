925c68b197a01257b042a5beb1bca78eb57019a2
chaihahaha <836275709@qq.com>
2018-01-15 00:39:58 +0800

    2018.1.15

--- main_stack_test_next_ptr.cpp
+++ main_stack_test_next_ptr.cpp
@@ -13,5 +13,19 @@ int main() {
     cout<<"pop value: "<<s.Pop()<<endl;
     cout<<endl;
     cout<<"isemp value: "<<s.IsEmpty()<<endl;
-        
+    StackNode p;
+    p.data =1;
+    StackNode *q=&p;
+    q=q+1;
+    
+    typedef struct vnode{
+        int data;
+    }vnode, adj[2];
+    adj r;
+    r[0].data=1;
+    cout<<r[0].data;
+    
+    *q = p; //不能用*直接对指针所指对象赋值？可以
+    //*q = 1; 不同类型不能赋值
+    //cout<<(*q)<<endl;这个自定义类型不能直接输出
 }
