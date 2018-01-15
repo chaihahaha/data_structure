929c2316d0ddd9163aca9b890cf437c011584272
chaihahaha <836275709@qq.com>
2018-01-15 01:09:41 -0500

    2018算法设计第一题

--- evenOddSort_2018.cpp
+++ evenOddSort_2018.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 void evenOddSort(int A[]) {
+    //要求：将数组中奇数元素移动到索引为奇数的位置，偶数元素移动到索引为偶数的位置。
+    //假设数组长度存放在了A[0]位置
     int i = 1;
     int j = 2;
     while(i <= A[0] && j <= A[0]) {
